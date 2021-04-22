c56407badb5a54def3ebffa3c1ee0e16c5c7a5f6
paraglande03 <paraglande03@gmail.com>
2021-04-22 16:52:04 +0530

    Added: Mapping for controller for Update method

--- src/main/java/com/parag/EmployeePayroll/controller/EmployeePayrollController.java
+++ src/main/java/com/parag/EmployeePayroll/controller/EmployeePayrollController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +32,10 @@ public class EmployeePayrollController {
 	@PostMapping("/create")
 	public ResponseEntity<String> addEmployyePayrollData(@RequestBody EmployeePayrollDTO empPayrollDTO){
 		return new ResponseEntity<String>("Created Emp DAta for: "+empPayrollDTO,HttpStatus.OK);
+	}
+	
+	@PutMapping("/update")
+	public ResponseEntity<String> updateEmployeePayrollData(@RequestBody EmployeePayrollDTO empPayrollDTO){
+		return new ResponseEntity<String> ("Update EmployeePayroll Data For: "+ empPayrollDTO,HttpStatus.OK);
 	}
 }
