193a4ef525daf92b56aabaa818ae178a28277a4d
paraglande03 <paraglande03@gmail.com>
2021-04-22 16:33:24 +0530

    Added: Mapping for controller for get by ID method

--- src/main/java/com/parag/EmployeePayroll/controller/EmployeePayrollController.java
+++ src/main/java/com/parag/EmployeePayroll/controller/EmployeePayrollController.java
@@ -4,6 +4,7 @@ import org.hibernate.internal.build.AllowSysOut;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,10 @@ public class EmployeePayrollController {
 	@RequestMapping({"","/","/get"})
 	public ResponseEntity<String> getEmployeePayrollData(){
 		return new ResponseEntity<String>("Get call success",HttpStatus.OK);
+	}
+	
+	@GetMapping("/get/{empId}")
+	public ResponseEntity<String> getEmployeePayrollData(@PathVariable("empId")int empId){
+		return new ResponseEntity<String>("Get call success For Id: "+empId,HttpStatus.OK);
 	}
 }
